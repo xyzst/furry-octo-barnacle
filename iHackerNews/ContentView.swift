17d182fa1d289e5fbb7d67d788f5479897571de6
Darren Rambaud <xyzst@users.noreply.github.com>
2020-03-23 22:38:20 -0500

    Initialize SwiftUI Project
    - Commit boilerplate code generated by Xcode
    
    Related to: none

---
+++ iHackerNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iHackerNews
+//
+//  Created by Darren Rambaud on 3/23/20.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Text("Hello, World!")
+            Text("??")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
